2fcf7d6036aff2179802c61b180714daa186786e
Maged Eshak <magedeshak39@gmail.com>
2024-12-23 11:06:37 +0200

    JS Installation

---
+++ function.js
@@ -0,0 +1,28 @@
+console.log("1- Function Intro And Basic Usage");
+console.log("-".repeat(20));
+
+function sayHello(userName) {
+    console.log(`Hello ${userName}`);
+}
+
+sayHello(`Maged`);
+
+console.log("-".repeat(20));
+console.log("2- Function Advanced Examples");
+console.log("-".repeat(20));
+
+console.log("-".repeat(20));
+console.log("3- Function Return Statement And Use Cases");
+console.log("-".repeat(20));
+
+console.log("-".repeat(20));
+console.log("4- Function Default Parameters");
+console.log("-".repeat(20));
+
+console.log("-".repeat(20));
+console.log("5- Function Rest Parameters");
+console.log("-".repeat(20));
+
+console.log("-".repeat(20));
+console.log("6- Function Ultimate Practice");
+console.log("-".repeat(20));
