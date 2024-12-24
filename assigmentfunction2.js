4c8accf5c63db1fbb077e02d71b80e2641db70e3
Maged Eshak <magedeshak39@gmail.com>
2024-12-24 22:22:44 +0200

    update

--- assigmentfunction2.js
+++ assigmentfunction2.js
@@ -8,22 +8,29 @@ console.log("----------------------");
 
 function getDetails(zName, zAge, zCountry) {
     function namePattern(zName) {
-    // Write Your Code Here
+        let [fName, lName] = zName.split(" ");
+        return `${fName} ${lName[0].toUpperCase()}.`;
+
     // Osama Mohamed => Osama M.
     // Ahmed ali => Ahmed A.
-        }
+    }
+
     function ageWithMessage(zAge) {
-    // Write Your Code Here
+        let age = parseInt(zAge);
+        return `Your Age Is ${age}`;
+
     // 38 Is My Age => Your Age Is 38
     // 32 Is The Age => Your Age Is 32
-        }
+    }
+    
     function countryTwoLetters(zCountry) {
-    // Write Your Code Here
+        let country = zCountry.slice(0, 2).toUpperCase();
+        return `You Live In ${country}`;
     // Egypt => You Live In EG
     // Syria => You Live In SY
-        }
+    }
     function fullDetails() {
-    // Write Your Code Here
+        return `Hello ${namePattern(zName)}, ${ageWithMessage(zAge)},  ${countryTwoLetters(zCountry)}`;
         }
     return fullDetails(); // Do Not Edit This
 }
@@ -35,7 +42,7 @@ console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
 // Hello Ahmed A., Your Age Is 32, You Live In SY
 
 console.log("----------------------");
-console.log("-- التكليف 02 --");
+console.log("-- ال تكليف 02 --");
 console.log("----------------------");
 
 // function itsMe() {
